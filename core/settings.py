164c1c6171dc50e06641607bb9ce46e27e3fddbe
makeprodigy <payneparihar13@gmail.com>
2025-05-26 05:21:22 +0530

    updated settings

--- core/settings.py
+++ core/settings.py
@@ -27,11 +27,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-your-secret-key'
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
 
-ALLOWED_HOSTS = []
+DEBUG = False
 
+ALLOWED_HOSTS = ['documentmodel-backend.onrender.com']
 
 # Application definition
 
